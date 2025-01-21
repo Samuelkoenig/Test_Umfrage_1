14de5bf835809e12312f1b9c712b9dff64da5dab
Samuel König <koenigsamuel99@gmx.de>
2025-01-21 14:01:53 +0100

    safari keyboard test

--- public/chatbot.js
+++ public/chatbot.js
@@ -129,10 +129,10 @@ function attachMobileChatbotEventListeners() {
     textarea.focus({ preventScroll: true });
     window.scrollTo(0, 0);
     scrollMessagesToBottom();
-    //setTimeout(() => { window.scrollTo(0,0); scrollMessagesToBottom() }, 50);
-    //setTimeout(() => { window.scrollTo(0,0); scrollMessagesToBottom() }, 100);
-    //setTimeout(() => { window.scrollTo(0,0); scrollMessagesToBottom() }, 200);
-    //setTimeout(() => { window.scrollTo(0,0); scrollMessagesToBottom() }, 300);
+    setTimeout(() => { window.scrollTo(0,0); scrollMessagesToBottom() }, 50);
+    setTimeout(() => { window.scrollTo(0,0); scrollMessagesToBottom() }, 100);
+    setTimeout(() => { window.scrollTo(0,0); scrollMessagesToBottom() }, 200);
+    setTimeout(() => { window.scrollTo(0,0); scrollMessagesToBottom() }, 300);
   });
 
   attachNoBounceListeners();
@@ -610,12 +610,12 @@ function updateVh() {
     const vh = window.visualViewport.height * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`);
 
-    //const offset = window.visualViewport.offsetTop;
-    //const chatbotInterface = document.getElementById('chatbot-interface');
+    const offset = window.visualViewport.offsetTop;
+    const chatbotInterface = document.getElementById('chatbot-interface');
     //const progressBar = document.getElementById('progress-bar');
-    //chatbotInterface.style.transform = `translateY(${offset}px)`;
+    chatbotInterface.style.transform = `translateY(${offset}px)`;
     //progressBar.style.transform = `translateY(${offset}px)`;
-    //scrollMessagesToBottom();
+    scrollMessagesToBottom();
 
   } else {
     const vh = window.innerHeight * 0.01;
