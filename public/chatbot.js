9dfc0688b166129db9bc19cf326303b2381be5ff
Samuel König <koenigsamuel99@gmx.de>
2025-01-23 16:25:46 +0100

    safari chatbot interface test

--- public/chatbot.js
+++ public/chatbot.js
@@ -619,13 +619,13 @@ function scrollMessagesToBottom() {
 
 
 function updateVh() {
-  /*if (window.visualViewport) {
+  if (window.visualViewport) {
     const vh = window.visualViewport.height * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`);
-  } else {*/
+  } else {
     const vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`);
-  //}
+  }
 }
 
 function alignChatbotUi() {
